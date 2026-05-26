b758920d370f1a55941be53b793bc1fdb54e12ca
Jhonata Carvalho <jhoncsilva28v@gmail.com>
2026-05-26 08:31:34 -0300

    alteração na mensagem de boas vindas

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('Boas vindas ao jogo do número secreto!');
 let numeroMaximo = 100;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
